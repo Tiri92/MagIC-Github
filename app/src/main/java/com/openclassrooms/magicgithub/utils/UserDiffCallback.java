396b6805cb74c5ab481fa3402d78a000f87a34b2
Thierry Bitar <bitarthierry@gmail.com>
2020-12-16 17:18:57 +0100

    correction du code du projet 2

---
+++ app/src/main/java/com/openclassrooms/magicgithub/utils/UserDiffCallback.java
@@ -0,0 +1,43 @@
+package com.openclassrooms.magicgithub.utils;
+
+import com.openclassrooms.magicgithub.model.User;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DiffUtil;
+
+public class UserDiffCallback extends DiffUtil.Callback {
+
+    private final List<User> oldUsers;
+    private final List<User> newUsers;
+
+    public UserDiffCallback(List<User> newUsers, List<User> oldUsers) {
+        this.newUsers = newUsers;
+        this.oldUsers = oldUsers;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldUsers.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+            return newUsers.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldUsers.get(oldItemPosition).getId() == newUsers.get(newItemPosition).getId();
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldUsers.get(oldItemPosition).equals(newUsers.get(newItemPosition));
+    }
+}
